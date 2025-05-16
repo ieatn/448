13e35d23d7f81eb984617b1448093239286d9bcd
ieatn <ieatnubcake@gmail.com>
2025-05-16 13:27:38 -0700

    folders

---
+++ server/app.py
@@ -0,0 +1,5 @@
+# create flask server
+# import depression csv data 
+# use linear regression and train the model on the data to predict percentage of depression based on survey
+# api route file send survey input and call this server for the prediction
+print('hello world')
