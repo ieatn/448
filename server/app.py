5e953cd72d6a5de0b7c5a6094ca51d971a023067
ieatn <ieatnubcake@gmail.com>
2025-05-21 11:25:29 -0700

    flask server

--- server/app.py
+++ server/app.py
@@ -3,3 +3,15 @@
 # use linear regression and train the model on the data to predict percentage of depression based on survey
 # api route file send survey input and call this server for the prediction
 print('hello world')
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
